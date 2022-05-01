8bc4537a3b13ac3d77d4af022b5ac1dec6ff0f97
Luis Antonio Momm Duarte <luismomm@gmail.com>
2022-05-01 17:57:47 -0300

    cabecalho (#1)

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -1,21 +1,31 @@
-import { Navbar, Nav, Container, NavbarBrand } from "react-bootstrap";
-import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import { Navbar, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <Navbar>
-      <Container style={{ backgroundColor: "blue" }}>
-        <NavbarCollapse id="responsive-navbar-nav">
-          <Nav>
-            <NavbarBrand> Abracos Abertos </NavbarBrand>
-            <Nav.Link href="about">About </Nav.Link>
-            <Nav.Link href="history">History </Nav.Link>
-            <Nav.Link href="volunteer">Volunteer </Nav.Link>
-            <Nav.Link href="contact">Contact Us </Nav.Link>
-          </Nav>
-        </NavbarCollapse>
-      </Container>
-    </Navbar>
+    <div
+      style={{
+        "text-align": "center",
+        padding: 5,
+      }}
+    >
+      <Navbar className="mr-auto">
+        <Nav class="justify-content-between">
+          <Navbar.Brand className="link-header" href="/">
+            <img src={logo} height="50" width="150" alt="logo" />
+          </Navbar.Brand>
+          <Nav.Link className="link-header" href="about">
+            Quem somos
+          </Nav.Link>
+          <Nav.Link className="link-header" href="volunteer">
+            Seja um voluntário
+          </Nav.Link>
+          <Nav.Link className="link-header" href="contact">
+            Contato
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    </div>
   );
 };
 
