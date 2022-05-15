d40e1eca4998ca364119d264d921c0c7056976b1
Santiago Fleitas <sansfleitas@gmail.com>
2022-05-15 20:59:35 -0300

    Desenvolvendo Footer (#10)
    * Criando footer
    
    * fix styles footer
    
    Co-authored-by: Luis Antonio Momm Duarte <luismomm@gmail.com>

---
+++ client/src/components/Footer/styles.js
@@ -0,0 +1,10 @@
+const styles = {
+  display: "flex",
+  justifyContent: "center",
+  backgroundColor: "#005298",
+  color: "#FFF",
+  fontSize: "0.8rem",
+  fontWeight: 600
+};
+
+export default styles;
