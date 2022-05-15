b2d582a19ecf29049d238d63eb7602185465efa0
Santiago Fleitas <sansfleitas@gmail.com>
2022-05-15 20:19:32 -0300

    fix styles footer

--- client/src/components/Footer/styles.js
+++ client/src/components/Footer/styles.js
@@ -1,8 +1,10 @@
-export default {
+const styles = {
   display: "flex",
   justifyContent: "center",
   backgroundColor: "#005298",
   color: "#FFF",
   fontSize: "0.8rem",
   fontWeight: 600
-}
+};
+
+export default styles;
