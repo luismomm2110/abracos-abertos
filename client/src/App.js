d4d57fbf23540bbd3f9dc487c8f52c51574306a7
Luis Antonio Momm Duarte <luismomm@gmail.com>
2022-05-19 20:24:54 -0300

    correcao import

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import { Home, About, History, Contact, Volunteer } from "./pages";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
