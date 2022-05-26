87959f67615e8a48f364c4f5c29af453ef73ec62
Santiago Fleitas <sansfleitas@gmail.com>
2022-05-26 19:23:57 -0300

    Adicionando quem somos na Home

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,16 +2,52 @@ import background from "./principal.jpeg";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <div
         style={{
           backgroundImage: `url(${background})`,
           height: "600px",
           width: "2000px",
+          backgroundPosition: "center center",
           backgroundRepeat: "no-repeat",
-          backgroundSize: "100%",
+          backgroundSize: "contain"
         }}
-      ></div>
-    </div>
+      >
+      </div>
+
+      <h2 style={{
+        textAlign: "center",
+        color: "#005298",
+        fontSize: "60px",
+        marginTop: "8rem",
+        marginBottom: "3rem"
+      }}>Quem Somos</h2>
+
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        marginBottom: "5rem"
+      }}>
+        <p style={{
+          display: "flex",
+          width: "30vw",
+          height: "30vh",
+          fontSize: "23px",
+          fontWeight: "300",
+          color: "#171717",
+          justifyContent: "center",
+          alignItems: "center",
+          marginRight: "3rem",
+        }}>
+            Este projeto acontece com a atuação comprometida e brilhante de professoras/es e  voluntárias/os  que com ações pedagógicas humanitárias, acolhedoras e, com os  “aBRaços abertos”, proporcionam novas formas de ver e estar na nova terra.
+            <br></br><br></br>
+            O projeto acontece na E.E.B. Professora Maria Amin Ghanem e visa contribuir para a inclusão social de crianças e adolescentes imigrantes, promovendo a aprendizagem da língua portuguesa, orientação nas atividades escolares, na conversação e o acolhimento com afeto, diálogo e escuta.
+        </p>
+        <img src="https://projetoabracosabertos.com.br/wp-content/uploads/2021/12/Colaboradoras-do-Abracos-Abertos.jpg" alt="Pessoas" style={{
+          width: "600px",
+          height: "400px"
+        }} />
+      </div>
+    </>
   );
 }
